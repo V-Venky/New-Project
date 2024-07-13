e80492dc3e7ddc232c052b551f8934d0dc83a237
V-Venky <140528393+V-Venky@users.noreply.github.com>
2024-07-13 19:55:25 +0530

    Update 6qrcode.py

--- 6qrcode.py
+++ 6qrcode.py
@@ -5,8 +5,4 @@ qr.add_data(data)
 qr.make(fit = True)
 img = qr.make_image(fill_color = 'blue',back_color = 'green')
 img.save('HTML.png')
-import qrcode 
-data = "HTML.py"
-qr = qrcode.QRCode(version = 1,box_size = 12,border = 5)
-qr.add_data(data)
-qr.make(fit = True)
+
